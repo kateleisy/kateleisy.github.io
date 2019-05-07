f0e2ef00f3feed6ff1b3fc9dd7467324ac8714b2
kateleisy <kateleisy@gmail.com>
2019-05-06 22:44:59 -0400

    all the building and changes to the celebrity site

---
+++ celebrity/next.js
@@ -0,0 +1,16 @@
+// const correctReply = document.querySelector("div.left-blue-message")
+// const correctBot = document.querySelector()
+
+
+// var questions = [
+//     "Hey what's up?",
+//     "Where are you",
+//     ""
+//   };
+  
+//   var currentQuestion = 0;
+  
+//   function showQuestion() {
+//     document.body.innerHTML += "<br>" + questions[currentQuestion];
+//     currentQuestion++;
+//   }
